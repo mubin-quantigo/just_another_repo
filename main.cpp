ca6f2a7bf396befdbb575f26f797621176fee6a7
mubin-quantigo <mubin.nezami@quantigo.ai>
2025-09-27 11:55:06 +0600

    - added function to sum the even numbers in an array

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@ int get_sum_of_odds(vector<int> nums){
     int sum = 0;
 
     for(int i = 0; i < nums.size(); i++){
+        
         if(nums[i] % 2){
+            sum += nums[i];
+        }
+    }
+
+    return sum;
+}
+
+int get_sum_of_evens(vector<int> nums){
+    int sum = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+
+        if(nums[i] % 2 == 0){
             sum += nums[i];
         }
     }
@@ -21,5 +35,8 @@ int main(){
 
     int sum_of_odd_elements = get_sum_of_odds(nums);
 
+    int sum_of_even_elements = get_sum_of_evens(nums);
+
     cout << "Sum of odd elements: " << sum_of_odd_elements << endl;
+    cout << "Sum of even elements: " << sum_of_even_elements << endl;
 }
