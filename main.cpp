b64db4d3f9b228eb43a9539bde9edf89442a4083
ssaayeedd <sayedsacc@gmail.com>
2025-09-27 12:20:35 +0600

    - added function to sum the numbers in an array

--- main.cpp
+++ main.cpp
@@ -29,6 +29,19 @@ int get_sum_of_evens(vector<int> nums){
     return sum;
 }
 
+int get_sum_of_all(vector<int> nums){
+    int sum = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+
+        sum += nums[i];
+        
+    }
+
+    return sum;
+
+}
+
 int main(){
 
     vector<int> nums = {11,2,3,8,5};
@@ -37,6 +50,10 @@ int main(){
 
     int sum_of_even_elements = get_sum_of_evens(nums);
 
+    int sum_of_all_elements = get_sum_of_all(nums);
+
     cout << "Sum of odd elements: " << sum_of_odd_elements << endl;
     cout << "Sum of even elements: " << sum_of_even_elements << endl;
+    cout << "Sum of all elements: " << sum_of_all_elements << endl;
+
 }
